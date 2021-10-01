227c087fa724f5ec328b7d72adde282e25594308
Emilio Haro <ermsharo@gmail.com>
2021-10-01 13:38:06 -0300

    upload changes

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,6 @@
-# test/application_system_test_case.rb
+=begin # test/application_system_test_case.rb
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
-end
+end =end
