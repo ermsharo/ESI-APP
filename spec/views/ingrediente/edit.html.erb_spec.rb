163e6952563b1850220d68e4ac366416d13054db
Emilio Haro <ermsharo@gmail.com>
2021-10-28 09:38:18 -0300

    change spec files

--- spec/views/ingrediente/edit.html.erb_spec.rb
+++ spec/views/ingrediente/edit.html.erb_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe "ingrediente/edit.html.erb", type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
--- spec/views/ingrediente/show.html.erb_spec.rb
+++ spec/views/ingrediente/show.html.erb_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe "ingrediente/show.html.erb", type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
